b231fdbd1d8d707f68ab70c4ed21b168c411d576
Manikanda Prabhu RaviChandran <rmkp@hotmail.com>
2023-07-24 21:21:34 +0530

    added test config

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ api.add_resource(Divide, "/division")
 
 @app.route('/')
 def hello_world():
-    return "Hello World! from GIT deployed app"
+    return "Hello World! from GIT deployed app - again to a node"
 
 
 if __name__=="__main__":
